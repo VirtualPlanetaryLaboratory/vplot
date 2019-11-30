9c95fbe6bff8c4c60082e9e2c4b9ea5be0fa1d69
Rodrigo Luger <rodluger@gmail.com>
2019-11-29 19:36:37 -0500

    bugfix in azure

--- .ci/update_json.sh
+++ .ci/update_json.sh
@@ -16,12 +16,13 @@ if [[ -n $BUILDREASON ]] && [[ $BUILDREASON != "PullRequest" ]]; then
     # Check if the JSON file changed
     if [[ $(git diff --name-only) == *"vplot/description.json"* ]]; then
 
+        # Clone the repo, update the file, and push it back
         mkdir tmp_vplot
         git clone https://github.com/VirtualPlanetaryLaboratory/vplot.git tmp_vplot
-        cp vplot/description.json tmp_vplot/vplot/description.json
         pushd tmp_vplot
             git checkout $GHBRANCH
             git pull
+            cp ../vplot/description.json vplot/description.json
             git add vplot/description.json
             git -c user.name='rodluger' -c user.email='rodluger@gmail.com' \
                 commit -m "update parameter descriptions"
