689a5ffc19ec8d61ed416305eb65ff15fa7929dc
rodluger <rodluger@gmail.com>
2021-05-13 19:51:28 +0000

    Deploying to gh-pages from @ VirtualPlanetaryLaboratory/vplot@8d2cb48d219357262ab437b44b4e48834f33449b 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.1.dev25+g564d01c',
+    VERSION: '1.0.1.dev1+g8d2cb48',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
